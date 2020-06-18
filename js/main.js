a892837fc60c940eabf8bf24310e831687db850c
Emmanuel Afolabi <emmanuel.afolabi@anakle.com>
2020-06-18 05:11:35 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+   $("#submit").click(function () {
+      // $("#formval").hide();
+      // $(".introtext").hide();
+      // $(".footertext").hide();
+      $("body").html($("#finalsignature").html());
+   });
+});
